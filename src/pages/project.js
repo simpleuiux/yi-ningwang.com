14d6c8933ee0de35f31567825a9f2aec1476f081
Annie Wang <annieynwang@gmail.com>
2020-02-08 16:19:22 -0800

    add particle.js

--- src/pages/project.js
+++ src/pages/project.js
@@ -8,48 +8,172 @@ import PostCard from "../components/postCard"
 // import "../utils/global.scss"
 import "../utils/normalize.css"
 import "../utils/css/screen.css"
+import Particles from 'react-particles-js'
+
 //TODO: switch to staticQuery, get rid of comments, remove unnecessary components, export as draft template
 const BlogIndex = ({ data }, location) => {
-    const siteTitle = data.site.siteMetadata.title
-    const posts = data.allMarkdownRemark.edges
-    let postCounter = 0
+  const siteTitle = data.site.siteMetadata.title
+  const posts = data.allMarkdownRemark.edges
+  let postCounter = 0
 
-    return (
-        <Layout title={siteTitle}>
-            <SEO
-                title="All posts"
-                keywords={[`blog`, `gatsby`, `javascript`, `react`]}
-            />
-            {/* <Bio /> */}
-            <article className="post-content page-template no-image">
-                <div className="post-content-body">
-                    <h1 id="clean-minimal-and-deeply-customisable-london-is-a-theme-made-for-people-who-appreciate-simple-lines-"><br />
-                        Clean, minimal, and modern UI/UX design. <br /> Follow me on {" "}
-                        <a href="https://dribbble.com/simpleuiux">
-                            dribbble
+  return (
+    <Layout title={siteTitle}>
+      <SEO
+        title="All posts"
+        keywords={[`blog`, `gatsby`, `javascript`, `react`]}
+      />
+
+      <Particles style={{ position: 'absolute' }}
+        params={{
+          particles: {
+
+            number: {
+              value: 60,
+              density: {
+                enable: true,
+                value_area: 800
+              }
+            },
+            color: {
+              value: '#bebebe'
+            },
+            shape: {
+              type: 'circle',
+              stroke: {
+                width: 0,
+                color: '#bebebe'
+              },
+              polygon: {
+                nb_sides: 4
+              },
+              image: {
+                src: '',
+                width: 100,
+                height: 100
+              }
+            },
+            opacity: {
+              value: 0.5,
+              random: false,
+              anim: {
+                enable: false,
+                speed: 1,
+                opacity_min: 0.1,
+                sync: false
+              }
+            },
+            size: {
+              value: 3,
+              random: true,
+              anim: {
+                enable: false,
+                speed: 40,
+                size_min: 0.1,
+                sync: false
+              }
+            },
+            line_linked: {
+              enable: true,
+              distance: 200,
+              color: '#eee',
+              opacity: 1,
+              width: 1
+            },
+            move: {
+              enable: true,
+              speed: 2,
+              direction: 'none',
+              random: false,
+              straight: false,
+              out_mode: 'out',
+              bounce: false,
+              attract: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 1200
+              }
+            },
+            array: []
+          },
+          interactivity: {
+            detect_on: 'canvas',
+            events: {
+              onhover: {
+                enable: true,
+                mode: 'repulse'
+              },
+              onclick: {
+                enable: true,
+                mode: 'push'
+              },
+              resize: true
+            },
+            modes: {
+              grab: {
+                distance: 400,
+                line_linked: {
+                  opacity: 1
+                }
+              },
+              bubble: {
+                distance: 400,
+                size: 40,
+                duration: 2
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4
+              },
+              push: {
+                particles_nb: 4
+              },
+              remove: {
+                particles_nb: 2
+              }
+            },
+            mouse: {}
+          },
+          retina_detect: true,
+          fn: {
+            interact: {},
+            modes: {},
+            vendors: {}
+          },
+          tmp: {}
+        }
+
+        } />
+
+      {/* <Bio /> */}
+      <article className="post-content page-template no-image">
+        <div className="post-content-body">
+          <h1 id="clean-minimal-and-deeply-customisable-london-is-a-theme-made-for-people-who-appreciate-simple-lines-"><br />
+            Clean, minimal, and modern UI/UX design. <br /> Follow me on {" "}
+            <a href="https://dribbble.com/simpleuiux">
+              dribbble
             </a>{" "}and{" "}
-                        <a href="https://medium.com/@simpleuiux">
-                            medium
+            <a href="https://medium.com/@simpleuiux">
+              medium
             </a>{" "}
-                    </h1>
-                </div>
-            </article>
-            <br /><br /><br /><br />
-            <div className="post-feed">
-                {posts.map(({ node }) => {
-                    postCounter++
-                    return (
-                        <PostCard
-                            key={node.fields.slug}
-                            count={postCounter}
-                            node={node}
-                            postClass={`post`}
-                        />
-                    )
-                })}
-            </div>
-        </Layout>
-    )
+          </h1>
+        </div>
+      </article>
+      <br /><br /><br /><br />
+      <div className="post-feed">
+        {posts.map(({ node }) => {
+          postCounter++
+          return (
+            <PostCard
+              key={node.fields.slug}
+              count={postCounter}
+              node={node}
+              postClass={`post`}
+            />
+          )
+        })}
+      </div>
+    </Layout>
+  )
 }
 
 const indexQuery = graphql`
@@ -86,10 +210,10 @@ const indexQuery = graphql`
 `
 
 export default props => (
-    <StaticQuery
-        query={indexQuery}
-        render={data => (
-            <BlogIndex location={props.location} props data={data} {...props} />
-        )}
-    />
+  <StaticQuery
+    query={indexQuery}
+    render={data => (
+      <BlogIndex location={props.location} props data={data} {...props} />
+    )}
+  />
 )
