78c1ea09eb9f2746c094712e3d3856a616681eab
Annie Wang <annieynwang@gmail.com>
2020-02-16 23:52:18 -0800

    siteconfig update

--- siteConfig.js
+++ siteConfig.js
@@ -1,9 +1,9 @@
 module.exports = {
-  name: 'Yining Wang',
-  shortName: 'Yining Wang',
-  description: 'Hi! I am Yining. I design ideas.',
-  url: 'https://simpleuiux.com', // Domain of your site without prefix!
-  prefix: '/',
-  author: 'Yining Wang',
-  twitter: 'Yining Wang',
+  name: "Yining Wang",
+  shortName: "Yining Wang",
+  description: "Hi! I am Yining. I design ideas.",
+  url: "https://yi-ningwang.com", // Domain of your site without prefix!
+  prefix: "/",
+  author: "Yining Wang",
+  twitter: "Yining Wang",
 }
