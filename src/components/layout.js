648089c4053ac361c877c4e9d07098f7b6aaa787
simpleuiux <annieynwang@gmail.com>
2023-11-17 10:20:47 -0500

    header

--- src/components/layout.js
+++ src/components/layout.js
@@ -13,99 +13,51 @@ const Layout = props => {
   return (
     <div className={`site-wrapper ${toggleNav ? `site-head-open` : ``}`}>
       <header className="site-head">
-        <div className="site-head-container">
-          <a
-            className="nav-burger"
-            href={`#`}
-            onClick={() => setToggleNav(!toggleNav)}
-          >
-            <div
-              className="hamburger hamburger--collapse"
-              aria-label="Menu"
-              role="button"
-              aria-controls="navigation"
-            >
-              <div className="hamburger-box">
-                <div className="hamburger-inner" />
-              </div>
-            </div>
-          </a>
-          <nav id="swup" class="site-head-right">
-            <ul className="nav" role="menu">
-              {/* <li className="nav-home nav-current" role="menuitem">
-                <Link to={`/`}>Project</Link>
-              </li> */}
-
-              <Link
-                className="site-head-logo"
-                to={`/`}
-                activeStyle={activeStyle}
-              >
-                annie yining wang
-              </Link>
-            </ul>
-          </nav>
-
-          <div className="site-head-left">
-            <div className="social-links">
-              <li className="nav-about" role="menuitem">
-              <Link to={`/about`} activeStyle={activeStyle}>
-                  ABOUT
-                </Link>
-              </li>
-              <a
-                href={Resume}
-                aria-label="Resume"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                {" "}
-                RESUME
-</a>
-              {/* <li className="nav-project" role="menuitem">
-                <Link to={`/project`} activeStyle={activeStyle}>
-                  PROJECTS
-                </Link>
-              </li> */}
-              {/* <li className="nav-contact" role="menuitem">
-                <Link to={`/contact`} activeStyle={activeStyle}>
-                  CONTACT
-                </Link>
-              </li> */}
-
-              {/* <li className="nav-elements" role="menuitem">
-                <Link to={`/elements`}>Elements</Link>
-              </li> */}
-
-              {/*    <a
-                href="https://www.facebook.com"
-                title="Facebook"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Facebook
-              </a>
-              <a
-                href="https://twitter.com"
-                title="Twitter"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Twitter
-              </a>
-               <Link 
-                to={`/rss.xml`}
-                title="RSS"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                RSS
-              </Link>
-              */}
-            </div>
+  <div className="site-head-container">
+    <nav id="swup" className="site-head-right">
+      <a
+        className="nav-burger"
+        href={`#`}
+        onClick={() => setToggleNav(!toggleNav)}
+      >
+        <div
+          className="hamburger hamburger--collapse"
+          aria-label="Menu"
+          role="button"
+          aria-controls="navigation"
+        >
+          <div className="hamburger-box">
+            <div className="hamburger-inner" />
           </div>
         </div>
-      </header>
+      </a>
+      <ul className="nav" role="menu">
+        <li className="nav-about" role="menuitem">
+          <Link to={`/about`} activeStyle={activeStyle}>
+            ABOUT
+          </Link>
+        </li>
+        <li className="nav-resume" role="menuitem">
+          <a
+            href={Resume}
+            aria-label="Resume"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            RESUME
+          </a>
+        </li>
+      </ul>
+    </nav>
+
+    <div className="site-head-left">
+      <div className="social-links">
+        {/* Other social links or elements */}
+      </div>
+    </div>
+  </div>
+</header>
+
       <main id="site-main" className="site-main">
         <div id="swup" className="transition-fade">
           {children}
